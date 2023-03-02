a7ceadcf6ac53c9320ab53a416e0150c2d0da782
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-01 21:31:06 -0300

    Inserção de dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,47 +1,61 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
-//Recuperar Dados
+//Inserir Dados
 public class Program {
-	
-	//Statement e ResultSet
+
+	// PreparedStatement, executeUpdate, Statement.RETURN_GENERATED_KEYS, getGeneratedKeys
 	public static void main(String[] args) {
 
-		// Conecta o banco
-		Connection connection = null;
-
-		// Prepara um consulta pra buscar todos os departamentos
-		Statement statement = null;
-
-		// Guarda o resultado da consulta
-		ResultSet resultSet = null;
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		PreparedStatement pst = null;
 		try {
+			conn = DB.getConnection();
 
-			connection = DB.getConnection();
-			statement = connection.createStatement();
+			pst = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
-			resultSet = statement.executeQuery("select * from department");
+			pst.setString(1, "Carl Purple");
+			pst.setString(2, "carl@gmail.com");
+			pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			pst.setDouble(4, 3000.0);
+			pst.setInt(5, 4);
+			
+			/* EXAMPLE 2: Retornando o no Id de departamento gerado!
+			*pst = conn.prepareStatement(
+			*"insert into department (Name) values ('D1'),('D2')",
+			*Statement.RETURN_GENERATED_KEYS);
+			*/
+			int rowsAffected = pst.executeUpdate();
 
-			while (resultSet.next()) {
-				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+			if (rowsAffected > 0) {
+				ResultSet rs = pst.getGeneratedKeys();
+				while (rs.next()) {
+					// indicando que quer o valor da primeira coluna
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
 			}
 
 		} catch (SQLException e) {
-			e.getStackTrace();
-		}
-		// é boa prática fechar manualmente, para evitar erros.
-		finally {
-			DB.closeResultSet(resultSet);
-			DB.closeStatement(statement);
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(pst);
 			DB.closeConnection();
 		}
 	}
-
 }
