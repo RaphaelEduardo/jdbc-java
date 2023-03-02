2f59d8596daa0d3a70817f4fbb8c14e7f76dcb8a
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-01 21:37:00 -0300

    Transacoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,40 +1,87 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
-// Deletar Dados
+// Transações
 public class Program {
 
-	// DbIntegrityException (excessão personalizada)
+	/* 
+	 * Garantir a consistência do DB com: 
+	 * ATOMICIDADE, CONSISTENCIA, ISOLAMENTO E DURABILIDADE	
+	 * 
+	 * setAutoCommit(false) - cada operação feita isoladamente
+	 * será confirmada automaticamente.
+	 * 
+	 * commit() - confirma a operação.
+	 * 
+	 * rollback() - desfaz oque já foi feito até o momento
+	 * (usado em caso de falha/problemas).
+	*/
 	public static void main(String[] args) {
 
 		Connection ct = null;
-		PreparedStatement pst = null;
+		Statement st = null;
 		
 		try {
 			ct = DB.getConnection();
+	
+			/*
+			 *  Não é para confirmar as atualizações automaticamente.
+			 *  Vai ficar pendente a uma confirmação explicita do programador
+			 */
+	
+			ct.setAutoCommit(false);
 			
-			pst = ct.prepareStatement(
-					"DELETE FROM department " + 
-					"WHERE (Id = ?)");
-					
-			//(pos da ?, e o valor) 
-			pst.setInt(1, 2);
+			st = ct.createStatement();
+	
+			int rows1 = st.executeUpdate(
+					"UPDATE seller "
+					+ "SET BaseSalary = 2090 "
+					+ "WHERE DepartmentId = 1");
 			
-			int rowsAffected = pst.executeUpdate();
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			/*
+			 * int x = 1;
+			 * if (x < 2) {
+			 *	throw new SQLException("Fake error!");
+			 * }
+			 */
+			
+			
+			int rows2 = st.executeUpdate(
+					"UPDATE seller "
+					+ "SET BaseSalary = 3090 "
+					+ "WHERE DepartmentId = 2");
+			
+			
+			/*
+			 *  Confirmação do explicita programador
+			 *  todo o bloco acima fica protegido por uma lógica de transação.
+			 */
+			ct.commit();
+			
+			System.out.println("rows1 " + rows1);
+			
+			
+			System.out.println("rows2 " + rows2);
 			
 		}
+		// Lógica para voltar a transação caso tenha sido interrompida no meio...
 		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				ct.rollback();
+				throw new DbException("Transaction rolled back! Caused by " + e.getMessage());
+			} catch (SQLException e1) {
+				// se der um erro no rollback
+				throw new DbException("Error trying to rollback! Caused by: " + e.getMessage());
+			}
 		}
 		finally {
-			DB.closeStatement(pst);
+			DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
